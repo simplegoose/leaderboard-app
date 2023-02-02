c1b47cf832cc19739f3ca378d9267cbdf3a2e5cb
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-02-02 19:27:15 +0300

    - Change the elements to be rendered to table rows instead of listitem

--- src/js/render.js
+++ src/js/render.js
@@ -1,23 +1,33 @@
 export const render = (data = []) => {
   const tableList = document.querySelector('#table-list');
+  const tbody = tableList.querySelector('tbody');
 
   let html = '';
 
-  data.forEach((score) => {
+  data.forEach((score, index) => {
     html += `
-      <li>${score.user}: ${score.score}</li>
+      <tr>
+        <td>${index + 1}</td>
+        <td>${score.user}</td>
+        <td>${score.score}</td>
+      </tr>
     `;
   });
 
-  tableList.innerHTML = html;
+  tbody.innerHTML = html;
 };
 
 export const addListItem = ({ user, score }) => {
-  const tableList = document.querySelector('#table-list');
+  const tableList = document.querySelector('#table-list tbody');
+  const tableRows = tableList.querySelectorAll('tr');
 
-  const listItem = document.createElement('li');
+  const listItem = document.createElement('tr');
 
-  listItem.innerHTML = `${user}: ${score}`;
+  listItem.innerHTML = `
+    <td>${tableRows.length + 1}</td>
+    <td>${user}</td>
+    <td>${score}</td>
+  `;
 
   tableList.appendChild(listItem);
 };
